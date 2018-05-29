35c9c01f84cccd9bc5aee8f3372aae6c31987f14
MrBlamo <36609641+MrBlamo420@users.noreply.github.com>
2018-05-28 21:15:37 -0600

    CSB Update

--- script.extendedinfo/resources/lib/TheMovieDB.py
+++ script.extendedinfo/resources/lib/TheMovieDB.py
@@ -13,7 +13,7 @@ from functools32 import lru_cache
 from WindowManager import wm
 import datetime
 
-TMDB_KEY = 'b9dac73f34df572d5e551a8a66cfeb87'
+TMDB_KEY = '6d506e6b0050d7009755ab562863d037'
 POSTER_SIZES = ["w92", "w154", "w185", "w342", "w500", "w780", "original"]
 LOGO_SIZES = ["w45", "w92", "w154", "w185", "w300", "w500", "original"]
 BACKDROP_SIZES = ["w300", "w780", "w1280", "original"]
@@ -22,7 +22,7 @@ STILL_SIZES = ["w92", "w185", "w300", "original"]
 HEADERS = {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
-    'User-agent': 'XBMC/17.6 ( phil65@kodi.tv )'
+    'User-agent': 'Kodi/17.6 ( phil65@kodi.tv )'
 }
 base_url = "http://image.tmdb.org/t/p/"
 poster_size = "w500"
